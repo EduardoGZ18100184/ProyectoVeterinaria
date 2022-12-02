64f98fde8d8075c6c2bd9e93d612f6989a96135f
Eduardo <L18100184@nlaredo.tecn.mx>
2022-12-01 22:24:55 -0600

    devuelve id_usuario

--- routes/mascotas/mascota.py
+++ routes/mascotas/mascota.py
@@ -3,6 +3,7 @@ from sqlalchemy import exc
 from models import Mascota
 from app import db,bcrypt
 from auth import tokenCheck
+from models import User #add 01-12-22
 
 appmascota = Blueprint('appmascota',__name__,template_folder="templates")
 
@@ -26,7 +27,7 @@ def registro():
 
 @appmascota.route('/mascotas', methods=['GET'])
 @tokenCheck
-def getUsers(usuario):
+def getAllPets(usuario):
     print(usuario)
     print(usuario['admin'])
     if usuario['admin']:
@@ -41,3 +42,28 @@ def getUsers(usuario):
             mascotaData['tipo'] = mascota.tipo
             output.append(mascotaData)
         return jsonify({'mascotas':output})
+
+
+@appmascota.route('/mascotas-user', methods=['GET'])
+@tokenCheck
+def getMascotasUser(usuario):
+    print(usuario)
+    print(usuario['admin'])
+    output = []
+    #mascotas = Mascota.query.all()
+    #userExists = User.query.filter_by(id=usuario['id']).first()
+    id_usuario = usuario['registered_on']
+    id_usuario = usuario['user_id']
+    print('ID DEL USUARIO:')
+    print(str(usuario['email']))
+    print(id_usuario)
+    # mascotas = Mascota.query.filter_by(user_id=usuario['id']).first()
+    # for mascota in mascotas:
+    #     mascotaData = {}
+    #     mascotaData['id'] = mascota.id
+    #     mascotaData['nombre'] = mascota.nombre
+    #     mascotaData['id_duenio'] = mascota.user_id
+    #     mascotaData['raza'] = mascota.raza
+    #     mascotaData['tipo'] = mascota.tipo
+    #     output.append(mascotaData)
+    return jsonify({'mascotas':output})
